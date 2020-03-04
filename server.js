f5b84f2e3f7f9a86bafa0f156f214661d5923593
spider-binbai <56785039+spider-binbai@users.noreply.github.com>
2020-03-04 19:53:55 +0800

    toheroku

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use('/graphql' , graphqlHTTP({
     graphiql: true
 }))
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build' ));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build' , 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`App listening to PORT ${PORT}`);
     
